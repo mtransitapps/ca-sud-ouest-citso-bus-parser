03ae8df1dbb4fdd5e3fc1978179cdb951527d668
Mathieu Méa <mathieu.mea@gmail.com>
2015-03-16 14:26:08 -0400

    Minor

--- src/org/mtransit/parser/ca_sud_ouest_citso_bus/SudOuestCITSOBusAgencyTools.java
+++ src/org/mtransit/parser/ca_sud_ouest_citso_bus/SudOuestCITSOBusAgencyTools.java
@@ -87,7 +87,7 @@ public class SudOuestCITSOBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public void setTripHeadsign(MRoute route, MTrip mTrip, GTrip gTrip) {
 		String stationName = cleanTripHeadsign(gTrip.trip_headsign);
-		int directionId = Integer.valueOf(gTrip.direction_id);
+		int directionId = gTrip.direction_id;
 		mTrip.setHeadsignString(stationName, directionId);
 	}
 
